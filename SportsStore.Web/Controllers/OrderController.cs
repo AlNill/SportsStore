717fe13a1cf9c47d39344c49b2274d8e8fe30a42
alek.nill@yandex.ru <alek.nill@yandex.ru>
2024-05-19 07:27:49 -0700

    1. Add admin controller 2. Add CRUD operation for product for admin 3. Change repositories for CRUD operation 4. Add serverside and unobtrusive validations

--- SportsStore.Web/Controllers/OrderController.cs
+++ SportsStore.Web/Controllers/OrderController.cs
@@ -14,6 +14,21 @@ public class OrderController : Controller
         _repository = repository;
     }
 
+    public ViewResult List() =>
+        View(_repository.Orders.Where(o => !o.Shipped));
+
+    [HttpPost]
+    public IActionResult MarkShipped(int orderID)
+    {
+        Order order = _repository.Orders.FirstOrDefault(o => o.OrderId == orderID);
+        if(order != null)
+        {
+            order.Shipped = true;
+            _repository.SaveOrder(order);
+        }
+        return RedirectToAction(nameof(List));
+    }
+
     public ViewResult Checkout() => View(new Order());
 
     [HttpPost]
--- SportsStore.Web/Models/EFProductRepository.cs
+++ SportsStore.Web/Models/EFProductRepository.cs
@@ -10,4 +10,33 @@ public class EFProductRepository : IProductRepository
     }
 
     public IQueryable<Product> Products => _context.Products;
+
+    public void SaveProduct(Product product)
+    {
+        if(product.ProductID == 0)
+            _context.Products.Add(product);
+        else
+        {
+            Product dbEntity = _context.Products.FirstOrDefault(p => p.ProductID == product.ProductID);
+            if(dbEntity != null)
+            {
+                dbEntity.Name = product.Name;
+                dbEntity.Description = product.Description;
+                dbEntity.Price = product.Price;
+                dbEntity.Category = product.Category;
+            }
+        }
+        _context.SaveChanges();
+    }
+
+    public Product DeleteProduct(int productID)
+    {
+        Product dbEntry = _context.Products.FirstOrDefault(p => p.ProductID == productID);
+        if(dbEntry != null)
+        {
+            _context.Products.Remove(dbEntry);
+            _context.SaveChanges();
+        }
+        return dbEntry;
+    }
 }
